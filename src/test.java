9b858c1cc84aec8625416c9ac9d7911376ee9727
GEORGI ZAHARIEV <GEORGI ZAHARIEV@DESKTOP-MRDPU1L>
2021-06-01 07:58:57 +0300

    Test

--- src/test.java
+++ src/test.java
@@ -10,28 +10,27 @@ public class test {
 		Node<Fraction> f = new Node<Fraction>(new Fraction(1, 2));
 		System.out.println(f.toString());
 
-		List<String> l = new List <String>(new String("Lunch"));
+		List<String> l = new List<String>(new String("Lunch"));
 		System.out.println(l.toString());
 		l.push_back(new String("Diner"));
 		System.out.println(l.toString());
 		l.push_front(new String("Breakfast"));
 		System.out.println(l.toString());
-		
+
 		l.pop_front();
 		System.out.println(l.toString());
-		
-		List<Integer> i = new List <Integer>();
-		for( int j = 10 ; j <=20 ; j++ ) i.push_back(j);
+
+		List<Integer> i = new List<Integer>();
+		for (int j = 10; j <= 20; j++)
+			i.push_back(j);
 		System.out.println(i.toString());
-		
-		 try{
-			   System.out.println(i.getData(6));
-			 }catch(Exception e){
-			   System.out.println("There are fewer elements");
-			   
-			 }
-		
-		
+
+		try {
+			System.out.println(i.getData(6));
+		} catch (Exception e) {
+			System.out.println("There are fewer elements");
+
+		}
 
 	}
 }
